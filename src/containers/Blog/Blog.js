29093bf4cbf54930de4df78ab5171c23811458ab
surya maulana <suryamaulana1980@gmail.com>
2020-04-10 07:21:50 +0700

    handling error localy

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import './Blog.css';
 class Blog extends Component {
     state = {
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
     }
 
     componentDidMount () {
@@ -24,7 +25,12 @@ class Blog extends Component {
                 });
                 this.setState({posts: updatedPosts});
                 // console.log(response);
+            })
+            .catch(error => {
+                this.setState({error: true});
+                // console.log(error);
             });
+
     }
 
     postSelectedHandler = (id) => {
@@ -32,13 +38,16 @@ class Blog extends Component {
     }
 
     render () {
-        const posts = this.state.posts.map(post => {
-            return <Post 
-                        key={post.id} 
-                        title={post.title} 
-                        author={post.author}
-                        clicked={() => this.postSelectedHandler(post.id)}/>
-        });
+        let posts = <p style={{textAlign: "center"}}>Something went wrong!</p>
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post 
+                            key={post.id} 
+                            title={post.title} 
+                            author={post.author}
+                            clicked={() => this.postSelectedHandler(post.id)}/>
+            });
+        }
 
         return (
             <div>
