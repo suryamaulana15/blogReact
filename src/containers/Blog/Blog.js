198cae997f317cff2d56a807064a3f78957adaff
surya maulana <suryamaulana1980@gmail.com>
2020-05-20 14:35:45 +0700

    netsed route

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,73 +1,42 @@
 import React, { Component } from 'react';
-// import axios from 'axios';
-import axios from '../../axios';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import { Route, NavLink, Switch } from 'react-router-dom';
+
+import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
+// import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false
-    }
-
-    componentDidMount () {
-        axios.get('/posts')
-            .then(response =>{
-                const posts = response.data.slice(0, 4);
-                const updatedPosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author: 'Max'
-                    }
-                });
-                this.setState({posts: updatedPosts});
-                // console.log(response);
-            })
-            .catch(error => {
-                this.setState({error: true});
-                // console.log(error);
-            });
-
-    }
-
-    postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
-    }
-
     render () {
-        let posts = <p style={{textAlign: "center"}}>Something went wrong!</p>
-        if (!this.state.error) {
-            posts = this.state.posts.map(post => {
-                return <Post 
-                            key={post.id} 
-                            title={post.title} 
-                            author={post.author}
-                            clicked={() => this.postSelectedHandler(post.id)}/>
-            });
-        }
-
         return (
             <div className="Blog">
                 <header>
                     <ul>
-                        <li><a href="/">home</a></li>
-                        <li><a href="new">New Post</a></li>
+                        <li><NavLink 
+                            to="/posts/" 
+                            exact
+                            activeClassName="my-active"
+                            activeStyle={{
+                                color: '#fa923f',
+                                textDecoration: 'underline'
+                            }}>home</NavLink></li>
+                        <li><NavLink to={{
+                            // pathname: this.props.match.url + 'new-post',
+                            pathname: '/new-post',
+                            hash: '#submit',
+                            search: '?quick-submit=true'
+                        }}>New Post</NavLink></li>
                     </ul>
                 </header>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost 
-                        id={this.state.selectedPostId}/>
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+                {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+                <Route path="/" render={() => <h1>Home 2</h1>} /> */}
+
+                
+                <Switch>
+                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/posts" component={Posts} />    
+                </Switch>
             </div>
         );
     }
--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -8,10 +8,19 @@ class FullPost extends Component {
         loadedPost: null
     }
 
+    componentDidMount () {
+        console.log(this.props);
+        this.loadData();
+    }
+
     componentDidUpdate () {
-        if (this.props.id) {
-            if ( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
-                axios.get('/posts/' + this.props.id)
+        this.loadData();
+    }
+
+    loadData() {
+        if (this.props.match.params.id) {                                          //integer with +
+            if ( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== +this.props.match.params.id)) {
+                axios.get('/posts/' + this.props.match.params.id)
                 .then(response => {
                     this.setState({loadedPost: response.data});
                     // console.log(response);
@@ -22,7 +31,7 @@ class FullPost extends Component {
     }
 
     deletePostHandler = () => {
-        axios.delete('/posts/' + this.props.id)
+        axios.delete('/posts/' + this.props.match.params.id)
             .then(response => {
                 console.log(response);
             });
@@ -31,7 +40,7 @@ class FullPost extends Component {
 
     render () {
         let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
-        if (this.props.id) {
+        if (this.props.match.params.id) {
             post = <p style={{textAlign: 'center'}}>Loading ...</p>;
         }
         if (this.state.loadedPost) {
